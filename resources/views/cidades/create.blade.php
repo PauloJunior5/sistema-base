894a6c05e36f299e7385dec2f0f617c6c693bdcb
Paulo Junior <pjunior.live@gmail.com>
2020-06-14 23:49:57 -0300

    Correção do dropdown - selecionar Estados conforme Pais

--- resources/views/cidades/create.blade.php
+++ resources/views/cidades/create.blade.php
@@ -138,7 +138,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script>
     $(document).ready(function() {
-        var url_atual = '<?php echo URL::to(' / '); ?>';
+        var url_atual = '<?php echo URL::to(''); ?>';
         $('#input-pais').change(function() {
             var id_pais = $(this).val();
             $.post(url_atual + '/cidade/getEstados', {
