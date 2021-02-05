7691fe79131cf2188cf2940546894ee879e4e33c
Paulo Junior <pjunior.live@gmail.com>
2021-02-05 19:53:33 -0300

    reorganização de scripts, adicionando @include

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -328,22 +328,42 @@
     });
 </script>
 
-<style>
-    .readonly {
-        background-color: #e9ecef;
-        opacity: 1;
-    }
 
-    .readonly:hover {
-        background-color: #e9ecef;
-        opacity: 1;
-    }
+<script>
 
-    .readonly:focus {
-        background-color: #e9ecef;
-        opacity: 1;
-    }
-    
-</style>
+var url_atual = '<?php echo URL::to(''); ?>';
+    $('.idCondição_pagamento-cliente').click(function() {
+        var id_condicao_pagamento = $(this).val();
+        $.ajax({
+            method: "GET",
+            url: url_atual + '/condicaoPagamento/show',
+            data: { id_condicao_pagamento : id_condicao_pagamento },
+            dataType: "JSON",
+            success: function(response){
+                console.log(response);
+                $('#id-condicao_pagamento-input').val(response.id);
+                $('#condicao_pagamento-input').val(response.condicao_pagamento);
+                $('#condicao_pagamentoModal').modal('hide')
+            }
+        });
+    });
 
+    var url_atual = '<?php echo URL::to(''); ?>';
+    $('.idForma_pagamento').click(function() {
+        var id_forma_pagamento = $(this).val();
+        $.ajax({
+            method: "POST",
+            url: url_atual + '/formaPagamento/show',
+            data: { id_forma_pagamento : id_forma_pagamento },
+            dataType: "JSON",
+            success: function(response){
+                $('#id-forma_pagamento-input').val(response.id);
+                $('#forma_pagamento-input').val(response.forma_pagamento);
+                $('#forma_pagamentoModal').modal('hide')
+            }
+        });
+    });
+
+</script>
+@include('includes.scripts.cliente-condicao-pagamento')
 @endsection
