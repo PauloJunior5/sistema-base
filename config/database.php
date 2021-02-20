d87c9d12de8ea0c91975afcb2c6b46587ace557f
Paulo Junior <pjunior.live@gmail.com>
2021-02-20 14:11:23 -0300

    removed old socket connection

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', '/var/run/mysqld/mysqld.sock'),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
