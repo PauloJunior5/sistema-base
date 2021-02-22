677bed8f862550c6d567dcb8bb20adcfc236bbb4
Paulo Junior <pjunior.live@gmail.com>
2021-02-22 00:00:25 -0300

    ajustes na controller de cidades

--- app/Http/Controllers/CidadeController.php
+++ app/Http/Controllers/CidadeController.php
@@ -2,43 +2,58 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Cidade;
+use App\Http\Requests\CidadeRequest;
+use App\Interfaces\CidadeInterface;
 
 class CidadeController extends Controller
 {
+    public function __construct(CidadeInterface $cidadeInterface)
+    {
+        $this->cidadeInterface = $cidadeInterface;
+    }
+    
     public function index()
     {
+        return $this->cidadeInterface->index();
     }
 
     public function create()
     {
+        return $this->cidadeInterface->create();
     }
 
-    public function store(Cidade $request)
+    public function store(CidadeRequest $request)
     {
+        return $this->cidadeInterface->store($request);
     }
 
-    public function show(Cidade $request)
+    public function show(CidadeRequest $request)
     {
+        return $this->cidadeInterface->show($request);
     }
 
     public function edit($id_cidade)
     {
+        return $this->cidadeInterface->edit($id_cidade);
     }
 
-    public function update(Cidade $request)
+    public function update(CidadeRequest $request)
     {
+        return $this->cidadeInterface->update($request);
     }
 
     public function destroy($id_cidade)
     {
+        return $this->cidadeInterface->destroy($id_cidade);
     }
 
-    public function createCidade(Cidade $request)
+    public function createCidade(CidadeRequest $request)
     {
+        return $this->cidadeInterface->createCidade($request);
     }
 
-    public function createCidadeMedico(Cidade $request)
+    public function createCidadeMedico(CidadeRequest $request)
     {
+        return $this->cidadeInterface->createCidadeMedico($request);
     }
 }
