2f09e171aae5f9cfcf03dcdd4151f40fd3db65c7
Paulo Junior <pjunior.live@gmail.com>
2021-03-04 19:47:23 -0300

    clear model - pais

--- app/Models/Pais.php
+++ app/Models/Pais.php
@@ -1,24 +1,5 @@
 <?php
 
-// namespace App\Models;
-
-// use Illuminate\Database\Eloquent\Model;
-
-// class Pais extends Model
-// {
-//     protected $table = 'paises';
-    
-//     protected $fillable = [
-//         'id', 'sigla', 'pais', 'updated_at', 'created_at'
-//     ];
-
-//     public function Estados()
-//     {
-//         return $this->hasMany('App\Estado', 'id_pais', 'id');
-//     }
-
-// }
-
 namespace App\Models;
 
 use App\Models\Model;
