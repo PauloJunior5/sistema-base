888b4ed2d0f489ce730ec05596f331ff960d947b
Paulo Junior <pjunior.live@gmail.com>
2021-03-06 13:53:08 -0300

    adicionada mensagem de erro que estava faltando ao retornar para o index de cidades

--- resources/views/cidades/index.blade.php
+++ resources/views/cidades/index.blade.php
@@ -10,14 +10,14 @@
                         <p class="card-category"> {{ __('Aqui você pode gerenciar cidades') }}</p>
                     </div>
                     <div class="card-body">
-                        @if (session('status'))
+                        @if (session('Success'))
                         <div class="row">
                             <div class="col-sm-12">
                                 <div class="alert alert-success">
                                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                         <i class="material-icons">close</i>
                                     </button>
-                                    <span>{{ session('status') }}</span>
+                                    <span>{{ session('Success') }}</span>
                                 </div>
                             </div>
                         </div>
