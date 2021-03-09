d81f114303a6d472d49fc74bb1fb12502942c744
Paulo Junior <pjunior.live@gmail.com>
2021-03-09 20:37:28 -0300

    refatorado para o Repository Patter - FornecedorRepository

--- app/Repositories/FornecedorRepository.php
+++ app/Repositories/FornecedorRepository.php
@@ -2,25 +2,80 @@
 
 namespace App\Repositories;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
 class FornecedorRepository
 {
     public function mostrarTodos()
     {
+        $fornecedores = DB::table('fornecedores')->get();
+        return $fornecedores;
     }
 
     public function adicionar($dados)
     {
+        $result = null;
+
+        DB::beginTransaction();
+        try {
+
+            $result = DB::table('fornecedores')->insert($dados);
+
+            DB::commit();
+
+        } catch (\Throwable $th) {
+
+            DB::rollBack();
+            Log::debug('Warning - Não foi possivel criar fornecedor ' . $th);
+
+        }
+        return $result;
     }
 
     public function findById(int $id)
     {
+        $fornecedor = DB::table('fornecedores')->where('id', $id)->first();
+        return $fornecedor;
     }
 
     public function atualizar($dados)
     {
+        $result = null;
+
+        DB::beginTransaction();
+        try {
+
+            $result = DB::table('fornecedores')->where('id', $dados['id'])->update($dados);
+
+            DB::commit();
+
+        } catch (\Throwable $th) {
+
+            DB::rollBack();
+            Log::debug('Warning - Não foi possivel editar fornecedor: ' . $th);
+
+        }
+        return $result;
     }
 
     public function remover($id)
     {
+        $result = null;
+
+        DB::beginTransaction();
+        try {
+
+            $result = DB::table('fornecedores')->where('id', $id)->delete();
+
+            DB::commit();
+
+        } catch (\Throwable $th) {
+
+            DB::rollBack();
+            Log::debug('Warning - Não foi possivel excluir fornecedor: ' . $th);
+
+        }
+        return $result;
     }
 }
