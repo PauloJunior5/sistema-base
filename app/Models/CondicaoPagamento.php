6ad9dbfd8b91f920af09a14de3c89ffb8ba427f0
Paulo Junior <pjunior.live@gmail.com>
2021-03-11 23:17:45 -0300

    refatoracao da model de condiçoes de pagamento

--- app/Models/CondicaoPagamento.php
+++ app/Models/CondicaoPagamento.php
@@ -1,12 +1,108 @@
 <?php
 
+$fillable = [
+    'condicao_pagamento', 'multa', 'juro', 'desconto', 'parcelas'
+];
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Model;
 
-class CondicaoPagamento extends Model
+class FormaPagamento extends Model
 {
-    protected $fillable = [
-        'condicao_pagamento', 'multa', 'juro', 'desconto', 'parcelas'
-    ];
+    protected string $condicaoPagamento;
+    protected float $multa;
+    protected float $juro;
+    protected float $desconto;
+    protected string $parcelas;
+
+
+    public function __construct()
+    {
+        $this->condicaoPagamento = '';
+        $this->multa = 0;
+        $this->juro = 0;
+        $this->desconto = 0;
+        $this->parcelas = '';
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get e Set Condição de Pagamento
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function getCondicaoPagamento(): string
+    {
+        return $this->condicaoPagamento;
+    }
+
+    public function setCondicaoPagamento($condicaoPagamento)
+    {
+        $this->condicaoPagamento = $condicaoPagamento;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get e Set Multa
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function getMulta(): float
+    {
+        return $this->multa;
+    }
+
+    public function setMulta($multa)
+    {
+        $this->multa = $multa;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get e Set Juro
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function getJuro(): float
+    {
+        return $this->juro;
+    }
+
+    public function setJuro($juro)
+    {
+        $this->juro = $juro;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get e Set Desconto
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function getDesconto(): float
+    {
+        return $this->desconto;
+    }
+
+    public function setDesconto($desconto)
+    {
+        $this->desconto = $desconto;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get e Set Parcelas
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function getParcelas(): string
+    {
+        return $this->parcelas;
+    }
+
+    public function setParcelas($parcelas)
+    {
+        $this->parcelas = $parcelas;
+    }
 }
