deb272376616bdb47d1148a066d9c5088bbd262e
Paulo Junior <pjunior.live@gmail.com>
2021-03-31 23:08:01 -0300

    refatorada model - exames

--- app/Models/Exame.php
+++ app/Models/Exame.php
@@ -2,14 +2,84 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Model;
 
 class Exame extends Model
 {
-    protected $table = 'exames';
-    
-    protected $fillable = [
-        'id', 'exame', 'valor', 'plano', 'categoria', 'updated_at', 'created_at'
-    ];
+    protected string $exame;
+    protected float $valor;
+    protected string $plano;
+    protected string $categoria;
 
+    public function __construct()
+    {
+        $this->exame = '';
+        $this->valor = 0;
+        $this->plano = '';
+        $this->categoria = '';
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get e Set Exame
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function getExame(): string
+    {
+        return $this->exame;
+    }
+
+    public function setExame(string $exame)
+    {
+        $this->exame = $exame;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get e Set Valor
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function getValor(): string
+    {
+        return $this->valor;
+    }
+
+    public function setValor(string $valor)
+    {
+        $this->valor = $valor;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get e Set Plano
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function getPlano(): string
+    {
+        return $this->plano;
+    }
+
+    public function setPlano(string $plano)
+    {
+        $this->plano = $plano;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get e Set Categoria
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function getCategoria(): string
+    {
+        return $this->categoria;
+    }
+
+    public function setCategoria(string $categoria)
+    {
+        $this->categoria = $categoria;
+    }
 }
