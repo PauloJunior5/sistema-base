b43955f85c177a1b86274a9c68f900b08186497b
Paulo Junior <pjunior.live@gmail.com>
2021-06-05 20:53:42 -0300

    ajustes na profundidade das modais de condição e forma de pagamento

--- resources/views/layouts/modais/chamada-modal/condicaoPagamento.blade.php
+++ resources/views/layouts/modais/chamada-modal/condicaoPagamento.blade.php
@@ -1,5 +1,5 @@
 <!-- Start Modal -->
-<div class="modal fade" id="condicaoPagamentoModal" tabindex="-1" role="dialog" aria-labelledby="condicaoPagamentoModal" aria-hidden="true" style="z-index: 99998">
+<div class="modal fade" id="condicaoPagamentoModal" tabindex="-1" role="dialog" aria-labelledby="condicaoPagamentoModal" aria-hidden="true" style="z-index: 99999">
     <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content">
             <div class="modal-body">
@@ -10,7 +10,7 @@
 </div>
 {{-- End Modal --}}
 <!-- Start Modal -->
-<div class="modal fade" id="condicao_pagamentoCreateModal" tabindex="-1" role="dialog" aria-labelledby="condicao_pagamentoCreateModal" aria-hidden="true" style="z-index: 99998">
+<div class="modal fade" id="condicao_pagamentoCreateModal" tabindex="-1" role="dialog" aria-labelledby="condicao_pagamentoCreateModal" aria-hidden="true" style="z-index: 99999">
     <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content">
             <div class="modal-body">
