eea8d4f4707c650adc1b537eca20964fa0e6b212
Paulo Junior <pjunior.live@gmail.com>
2021-10-04 01:42:18 -0300

    adicionada categoria na sidebar

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -105,7 +105,12 @@
                                 <span class="sidebar-normal">{{ __('Exames') }} </span>
                             </a>
                         </li>
-                       
+                        <li class="nav-item {{ $activePage == 'categoria-management' ? ' active' : '' }}">
+                            <a class="nav-link" href="{{ route('categoria.index') }}">
+                                <span class="sidebar-mini"> C </span>
+                                <span class="sidebar-normal">{{ __('Categorias') }} </span>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </li>
