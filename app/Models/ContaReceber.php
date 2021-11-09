49374ba78115b9c6d585bb1d5237f18cbf6f4d7b
PauloJunior5 <pjunior.live@gmail.com>
2021-11-08 22:31:53 -0300

    incluida model de contas a receber

---
+++ app/Models/ContaReceber.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Model;
+
+class ContaReceber extends Model
+{
+    protected $pais;
+    protected $sigla;
+
+    public function __construct()
+    {
+        $this->pais = '';
+        $this->sigla = '';
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get e Set País
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function getPais(): string
+    {
+        return $this->pais;
+    }
+
+    public function setPais(string $pais)
+    {
+        $this->pais = $pais;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get e Set Sigla
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function getSigla(): string
+    {
+        return $this->sigla;
+    }
+
+    public function setSigla(string $sigla)
+    {
+        $this->sigla = $sigla;
+    }
+}
